6b901ba47589a9a832cea75ab1a8ceb438901fc7
Crowdbotics <team@crowdbotics.com>
2021-12-16 00:33:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bierdeckel_32444",
+  url: "https://bierdeckel_32444.botics.co",
+  api: "https://bierdeckel_32444.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
